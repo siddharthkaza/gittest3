eb7f0cea6e43eabf1326ea6ba59cbff48ac7e40b
skaza <skaza@towson.edu>
2012-10-16 10:58:45 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gittest3::Application.config.secret_token = '5bbd9bb9000c3d38fefd4a97858bf2a5878c5b0ce2d2cb973e8aa7ed5c1a49e339f7ada4a6c63acdf2df63b9a41518d135bc05d825146a3ecbbefddb6001f540'
